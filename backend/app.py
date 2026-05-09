4fc86204f3f3e5ec2a751c4666451d177194e1b4
panduramcharan12 <panduramcharan12@gmail.com>
2026-05-09 10:15:16 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -12,14 +12,22 @@ app.config['SECRET_KEY'] = 'ipl_secret_key'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
+# LOGIN PAGE
 @app.route("/")
 def login():
     return render_template("login.html")
 
 
+# MAIN LOBBY PAGE
 @app.route("/home")
 def home():
     return render_template("index.html")
+
+
+# ROOM PAGE
+@app.route("/room")
+def room():
+    return render_template("room.html")
 
 
 if __name__ == "__main__":
